2a351774bdf006039c852a48db669cbc3b7c0e26
Diffuse <48339639+diffuse@users.noreply.github.com>
2020-06-20 15:06:27 -0400

    Add handlers and counter routes

--- handlers.go
+++ handlers.go
@@ -1,1 +1,42 @@
 package gloss
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+	"strconv"
+)
+
+// Increment the value of the counter with ID counterId
+func IncrementCounterById(w http.ResponseWriter, r *http.Request) {
+	// get the counter ID
+	counterId, err := strconv.ParseUint(chi.URLParam(r, "counterId"), 10, 32)
+	if err != nil {
+		http.Error(w, "invalid counter ID", http.StatusBadRequest)
+		return
+	}
+
+	// increment in db
+	if err := IncrementCounter(int(counterId)); err != nil {
+		http.Error(w, "failed to increment counter value", http.StatusInternalServerError)
+	}
+}
+
+// Get the value of the counter with ID counterId
+func GetCounterById(w http.ResponseWriter, r *http.Request) {
+	// get the counter ID
+	counterId, err := strconv.ParseUint(chi.URLParam(r, "counterId"), 10, 32)
+	if err != nil {
+		http.Error(w, "invalid counter ID", http.StatusBadRequest)
+		return
+	}
+
+	// get value and return to requester
+	val, err := GetCounterVal(int(counterId))
+	if err != nil {
+		http.Error(w, "failed to get counter value", http.StatusInternalServerError)
+	}
+
+	if _, err := w.Write([]byte(strconv.Itoa(val))); err != nil {
+		panic(err)
+	}
+}
--- routes.go
+++ routes.go
@@ -4,6 +4,8 @@ import "github.com/go-chi/chi"
 
 func GetRoutes() *chi.Mux {
 	r := chi.NewRouter()
+	r.Post("/counter/{counterId}", IncrementCounterById)
+	r.Get("/counter/{counterId}", GetCounterById)
 
 	return r
 }
